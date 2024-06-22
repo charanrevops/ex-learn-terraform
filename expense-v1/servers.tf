df3da22b63fe15eca494310d6a99a9e02ed103bc
Charan Reddy <kothacharanreddy@gmail.com>
2024-06-22 12:19:58 +0200

    Learn Terraform

--- expense-v1/servers.tf
+++ expense-v1/servers.tf
@@ -6,3 +6,21 @@ resource "aws_instance" "frontend-front" {
     Name = "frontend"
   }
 }
+
+resource "aws_instance" "backend" {
+  ami           = "ami-031d574cddc5bb371"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-031d574cddc5bb371"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
