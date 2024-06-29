58a2b855b79ef7f6763d5b7d97aae37a78ef614d
Charan Reddy <kothacharanreddy@gmail.com>
2024-06-29 11:26:33 +0200

    Loops in terraform

--- expense-v2/main.tf
+++ expense-v2/main.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "ec2-nodes" {
   count         = length(var.components)
+  ami           = "ami-031d574cddc5bb371"
   instance_type = "t2.micro"
   vpc_security_group_ids =  ["sg-076b871708f2b2227"]
 
