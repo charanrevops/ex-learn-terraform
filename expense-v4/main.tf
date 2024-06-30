d8c2fb7a0ad87fb6dac8715f803fd8cb8443e0fb
Charan Reddy <kothacharanreddy@gmail.com>
2024-06-30 12:02:20 +0200

    Loops in terraform

--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -12,10 +12,10 @@ resource "aws_instance" "ec2-nodes" {
 variable "components" {
   default = {
     frontend = {
-      instance_type = "t2.micro"
+      instance_type = #"t2.micro"
     }
     backend = {
-      instance_type = "t2.micro"
+      instance_type = #"t2.micro"
     }
     mysql = {
       instance_type = "t2.micro"
