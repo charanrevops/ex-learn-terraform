97285ad258e523dd265a40abca432853a0edc6ee
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-04 09:34:31 +0200

    Loops in terraform

---
+++ expense-v5/variables.tf
@@ -0,0 +1,22 @@
+variable "components" {
+  default = {
+    frontend = {
+      instance_type="t3.small"
+    }
+    backend = {
+      instance_type="t3.small"
+    }
+    mysql = {
+      instance_type = "t2.micro"
+    }
+  }
+}
+
+
+variable "ami" {
+  default = "ami-041e2ea9402c46c32"
+}
+
+variable "vpc_security_group_ids"{
+  default = ["sg-076b871708f2b2227"]
+        }
