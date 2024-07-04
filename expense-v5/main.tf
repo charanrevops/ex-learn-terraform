3f0ebe12d88dd1ac9d29ce07ece861d08db139bc
Charan Reddy <kothacharanreddy@gmail.com>
2024-07-04 10:20:54 +0200

    Loops in terraform

--- expense-v5/main.tf
+++ expense-v5/main.tf
@@ -1,9 +1,9 @@
 module "app"{
   for_each = var.components
   source="./app"
-  ami   = var.ami
+  ami   = data.aws_ami.image_id
   instance_type =each.value["instance_type"]
   name = each.key
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   zone_id = data.aws_route53_zone.main.zone_id
 }
--- expense-v5/variables.tf
+++ expense-v5/variables.tf
@@ -13,13 +13,13 @@ variable "components" {
 }
 
 
-variable "ami" {
-  default = "ami-041e2ea9402c46c32"
-}
-
-variable "vpc_security_group_ids"{
-  default = ["sg-076b871708f2b2227"]
-        }
-variable "zone_id" {
-  default = "Z07982232LM1PBSHA15UZ"
-}
+# variable "ami" {
+#   default = "ami-041e2ea9402c46c32"
+# }
+#
+# variable "vpc_security_group_ids"{
+#   default = ["sg-076b871708f2b2227"]
+#         }
+# variable "zone_id" {
+#   default = "Z07982232LM1PBSHA15UZ"
+# }
